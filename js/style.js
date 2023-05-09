4bf1c1f6f101ea79bcac1e90986bbbe20cc1b475
Thai Son <nogthaison@gmail.com>
2023-05-09 17:18:51 +0700

    2nd commit

---
+++ js/style.js
@@ -0,0 +1,14 @@
+//aside
+$(document).ready(function(){
+    $('.aside-header-list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    nextArrow: false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 400000,
+    dotClass: 'slick-dots'
+    });
+  });
